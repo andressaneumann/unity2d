08b744f12afdc05f628761a4f9ef64e6dcb03396
Andressa Neumann <neumm.andressa@gmail.com>
2017-03-15 16:39:36 -0300

    second commit - platforms

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	private const float SkinWidth = .02f;
+	private const int TotalHorizontalRays = 8;
+	private const int TotalVerticalRays = 4;
+
+	private static readonly float SlopeLimitTangant = Mathf.Tan (75f * Mathf.Deg2Rad);
+
+	public LayerMask PlatformMask;
+	public ControllerParameters2D DefaltParameters;
+
+	public ControllerState2D State{ get; private set;}
+
+	public void Awake(){
+
+	}
+
+	public void AddForce(Vector2 force){
+
+	}
+
+	public void SetForce(Vector2 force){
+	
+	
+	}
+
+
+
+}
---
+++ Assets/Scripts/ControllerParameters2D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ControllerParameters2D {
+
+	public enum JumpBehavior{
+		CanJumpOnGround,
+		CanJumpAnywhere,
+		CantJump
+	}
+
+	public Vector2 MaxVelocity = new Vector2(float.MaxValue, float.MaxValue);
+
+	[Range(0, 90)]
+	public float SlopeLimit = 30;
+
+	public float Gravity = -25f;
+
+	public JumpBehavior JumpRestrictions;
+	public float JumpFrequency = .25f;
+
+}
